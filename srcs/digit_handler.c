b0579c52dae8960ef8148937bd5e9227635c0ecb
Mario Martín Moreno <mario.martin.moreno.2010@gmail.com>
2020-07-28 02:27:26 +0200

    Reworked %d and %i from scratch

--- srcs/digit_handler.c
+++ srcs/digit_handler.c
@@ -6,7 +6,7 @@
 /*   By: mmartin- <mmartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/28 00:28:06 by mmartin-          #+#    #+#             */
-/*   Updated: 2020/07/28 00:37:43 by mmartin-         ###   ########.fr       */
+/*   Updated: 2020/07/28 02:08:43 by mmartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,40 +21,37 @@ static char	sign_picker(t_flag const *flag, int *num)
 {
 	char	sign;
 
-	sign = flag->space ? ' ' : 0;
-	sign = flag->plus ? '+' : sign;
+	sign = 0;
 	if (*num < 0)
 	{
 		*num = -*num;
 		sign = '-';
 	}
+	else if (flag->plus)
+		sign = '+';
+	else if (flag->space)
+		sign = ' ';
 	return (sign);
 }
 
-int			int_handle(char *out, t_flag *flag, int num)
+int			int_handle(char *out, t_flag *flag, int n)
 {
-	int				sent;
-	int				sentptr;
-	int				nl;
-	char			sn;
-	unsigned char	is_exc;
+	int		sent;
+	int		zero;
+	char	sn;
 
 	sent = 0;
-	nl = ft_countdigits(num);
-	sn = sign_picker(flag, &num);
-	is_exc = !num && !flag->prec;
+	sn = sign_picker(flag, &n);
+	zero = flag->prec > ft_logn(10, n) ? flag->prec - ft_logn(10, n) - 1 : 0;
 	if (flag->zero && flag->prec < 0)
-		flag->prec = flag->width - (sn > 0);
-	flag->prec = flag->prec > nl ? flag->prec - nl + (sn == '-') : 0;
-	while (!flag->left && sent < flag->width - flag->prec -
-				nl - (flag->zero) + (sn != '-') + is_exc)
+		zero = flag->width - ft_logn(10, n) - !!sn - 1;
+	while (!flag->left && sent < flag->width - !!sn - zero - ft_logn(10, n) - 1)
 		*(out + sent++) = ' ';
 	if (sn)
 		*(out + sent++) = sn;
-	sentptr = -1;
-	while (++sentptr < flag->prec)
+	while (zero-- > 0)
 		*(out + sent++) = '0';
-	sent += is_exc ? 0 : ft_itoa_base(out + sent, num, "0123456789");
+	sent += !n && !flag->prec ? 0 : ft_itoa_base(out + sent, n, "0123456789");
 	while (flag->left && sent < flag->width)
 		*(out + sent++) = ' ';
 	return (sent);
